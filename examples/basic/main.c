03c2f0f52bd04b30b54f38fa279bc0ec6c4166e4
Thomas Ingebretse <thomas@maritimerobotics.com>
2015-05-01 12:53:28 +0200

    Basic example error checking.

--- examples/basic/main.c
+++ examples/basic/main.c
@@ -11,8 +11,14 @@ int main(int argc, char **argv)
 		"1 25544U 98067A   12317.22408565  .00013449  00000-0  23146-3 0  6331", 
 		"2 25544  51.6464 106.0986 0015548 274.8897  81.8006 15.51295927801004" };
 
+	// Create orbit object
 	orbit_t *iss = orbit_create(tle);
+	if (!iss) {
+		fprintf(stderr, "Failed to initialize orbit from tle!");
+		exit(1);
+	}
 
+	// Predict orbit
 	int i;
 	for (i=0;i<100;++i) {
 		
