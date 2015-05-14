c0ae177d9e96b190801ac949daef057d2db79d87
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-05-14 19:17:32 +0200

    Specified that julian dates are always in UTC.

--- include/predict/juliandate.h
+++ include/predict/juliandate.h
@@ -6,10 +6,10 @@
 /* The representation of time used by libpredict: The number of days since 31Dec79 00:00:00 UTC. */
 typedef double predict_julian_date_t;
 
-/* Convert time_t to julian date. */
+/* Convert time_t to julian date in UTC. */
 predict_julian_date_t predict_get_julian_date_from_time(time_t time);
 
-/* Convert julian date back to a time_t. */
+/* Convert julian date in UTC back to a time_t. */
 time_t predict_get_time_from_julian_date(predict_julian_date_t date);
 
 
