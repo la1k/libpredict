40c81ecd9147670fd7017330cb21f5f307eaaa6c
Asgeir Bjørgan <asgeirbj@samfundet.no>
2017-04-24 00:40:45 +0200

    Reorganize and reformat constant documentation.

--- src/defs.h
+++ src/defs.h
@@ -10,47 +10,84 @@
 //upper inclination for geosynchronous satellites
 #define GEOSYNCHRONOUS_INCLINATION_THRESHOLD_DEGREES 70
 
-#define PI		3.14159265358979323846	/* Pi */
-#define PI_HALF 	1.57079632679489656	/* Pi/2 */
-#define TWO_PI          6.28318530717958623     /* 2*Pi  */
-#define THREE_PI_HALF	4.71238898038468967	/* 3*Pi/2 */
-#define TWO_THIRD	6.6666666666666666E-1	/* 2/3 */
-#define J3_HARMONIC_WGS72 -2.53881E-6		/* J3 Harmonic (WGS '72) */
-#define EARTH_RADIUS_KM_WGS84	6.378137E3		/* WGS 84 Earth radius km */
-#define MINUTES_PER_DAY		1.44E3			/* Minutes per day */
-#define FLATTENING_FACTOR 3.35281066474748E-3	/* Flattening factor */
-#define SECONDS_PER_DAY	8.6400E4	/* Seconds per day */
-#define EARTH_ROTATIONS_PER_SIDERIAL_DAY 1.00273790934	/* Earth rotations/siderial day */
-#define SOLAR_RADIUS_KM 6.96000E5       /* Solar radius - km (IAU 76) */
-#define ASTRONOMICAL_UNIT_KM 1.49597870691E8 /* Astronomical unit - km (IAU 76) */
-#define NAUTICAL_TWILIGHT_SUN_ELEVATION -12.0 /* Upper elevation threshold for nautical twilight */
-#define AOSLOS_HORIZON_THRESHOLD 0.3 /* threshold for fine-tuning of AOS/LOS */
-
 #include <float.h>
 
-#define MAXELE_TIME_EQUALITY_THRESHOLD FLT_EPSILON /* Threshold used for comparing lower and upper brackets in find_max_elevation */
+/* Mathematical convenience constants used by sgp4/sdp4 and related routines */
+// pi
+#define PI		3.14159265358979323846
+// pi/2
+#define PI_HALF 	1.57079632679489656
+// 2*pi
+#define TWO_PI          6.28318530717958623
+// 3*pi/2
+#define THREE_PI_HALF	4.71238898038468967
+// 2/3
+#define TWO_THIRD	6.6666666666666666E-1
 
-#define MAXELE_MAX_NUM_ITERATIONS 10000 /* Maximum number of iterations in find_max_elevation */
+/* Time convenience constants */
+//Number of minutes per day
+#define MINUTES_PER_DAY	1.44E3
+//Number of seconds per day
+#define SECONDS_PER_DAY	8.6400E4
+///Difference between libpredict's predict_julian_date_t and the julian time format used in some of the internal functions
+#define JULIAN_TIME_DIFF 2444238.5
 
-#define SPEED_OF_LIGHT 299792458.0 /* doppler calculation */
+/* Physical properties */
+//J3 Harmonic (WGS '72)
+#define J3_HARMONIC_WGS72 			-2.53881E-6
+//WGS 84 Earth radius km
+#define EARTH_RADIUS_KM_WGS84			6.378137E3
+//Flattening factor
+#define FLATTENING_FACTOR 			3.35281066474748E-3
+//Earth rotations per siderial day
+#define EARTH_ROTATIONS_PER_SIDERIAL_DAY	1.00273790934
+//Solar radius in kilometers (IAU 76)
+#define SOLAR_RADIUS_KM 			6.96000E5
+//Astronomical unit in kilometers (IAU 76)
+#define ASTRONOMICAL_UNIT_KM 			1.49597870691E8
+//Upper elevation threshold for nautical twilight
+#define NAUTICAL_TWILIGHT_SUN_ELEVATION 	-12.0
+//Speed of light in vacuum
+#define SPEED_OF_LIGHT				299792458.0
+//Angular velocity of Earth in radians per seconds
+#define EARTH_ANGULAR_VELOCITY			7.292115E-5
 
-#define EARTH_ANGULAR_VELOCITY                7.292115E-5 /* Angular velocity of Earth in radians per seconds */
+/* Iteration constants */
+//Threshold used for fine-tuning of AOS/LOS
+#define AOSLOS_HORIZON_THRESHOLD 	0.3
+//Threshold used for comparing lower and upper brackets in find_max_elevation
+#define MAXELE_TIME_EQUALITY_THRESHOLD 	FLT_EPSILON
+//Maximum number of iterations in find_max_elevation
+#define MAXELE_MAX_NUM_ITERATIONS 	10000
 
-/* Spacetrack report #3 constants also used outside of SGP4/SDP4 code.
- * The constants/symbols are defined on page 76 to page 78 in the report. */
-#define XKE		7.43669161E-2 /* k_e = sqrt(Newton's universal gravitational * mass of the Earth), in units (earth radii per minutes)^3/2 */
-#define CK2		5.413079E-4 /* Corresponds to 1/2 * J_2 * a_E^2. J_2 is the second gravitational zonal harmonic of Earth, a_E is the equatorial radius of Earth. */
+/**
+ * Spacetrack report #3 constants also used outside of SGP4/SDP4 code.
+ * The constants/symbols are defined on page 76 to page 78 in the report.
+ **/
+//k_e = sqrt(Newton's universal gravitational * mass of the Earth), in units (earth radii per minutes)^3/2
+#define XKE		7.43669161E-2
+//Corresponds to 1/2 * J_2 * a_E^2. J_2 is the second gravitational zonal harmonic of Earth, a_E is the equatorial radius of Earth.
+#define CK2		5.413079E-4
 
-/* Spacetrack report #3 constants used only by SGP4/SDP4 code.
- * The constants/symbols are defined on page 76 to page 78 in the report. */
-#define E6A		1.0E-6 /* Shorthand for 10^-6. */
-#define AE		1.0 /* Distance units / Earth radii. */
-#define CK4		6.209887E-7 /* Corresponds to -3/8 * J_4 * a_E^4, where J_4 is the fourth gravitational zonal harmonic of Earth. */
-#define S_DENSITY_PARAM	1.012229 /* Parameter for SGP4/SGP8 density function. */
-#define QOMS2T		1.880279E-09 /* Corresponds to (q_0 - s)^4 in units (earth radii)^4, where q_0 and s are parameters for the SGP4/SGP8 density function. */
+/**
+ * Spacetrack report #3 constants used only by SGP4/SDP4 code.
+ * The constants/symbols are defined on page 76 to page 78 in the report.
+ **/
+//Shorthand for 10^-6.
+#define E6A		1.0E-6
+//Distance units / Earth radii.
+#define AE		1.0
+//Corresponds to -3/8 * J_4 * a_E^4, where J_4 is the fourth gravitational zonal harmonic of Earth.
+#define CK4		6.209887E-7
+//Parameter for SGP4/SGP8 density function.
+#define S_DENSITY_PARAM	1.012229
+//Corresponds to (q_0 - s)^4 in units (earth radii)^4, where q_0 and s are parameters for the SGP4/SGP8 density function.
+#define QOMS2T		1.880279E-09
 
-/* Constants in deep space subroutine. Not defined in spacetrack report #3.
- * The constants might be defined in Hujsak (1979) and Hujsak and Hoots (1977). */
+/**
+ * Constants in deep space subroutine. Not defined in spacetrack report #3.
+ * The constants might be defined in Hujsak (1979) and Hujsak and Hoots (1977).
+ **/
 #define zns		1.19459E-5
 #define c1ss		2.9864797E-6
 #define zes		1.675E-2
@@ -75,13 +112,5 @@
 #define root52		1.1428639E-7
 #define root54		2.1765803E-9
 #define thdt           4.3752691E-3
-
-
-
-
-/* Difference between libpredict's predict_julian_date_t and the julian time
- * format used in some of the internal functions */
-#define JULIAN_TIME_DIFF 2444238.5
-
 
 #endif
