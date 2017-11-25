e7c10b17b93467bb28aa823e815c499d78369070
Asgeir Bjørgan <asgeirbj@samfundet.no>
2017-11-25 16:51:02 +0100

    Use proper doxygen grouping in defs.h.

--- src/defs.h
+++ src/defs.h
@@ -3,9 +3,10 @@
 
 #include <float.h>
 
-/**
- * Definitions for geosynchronous orbits.
- **/
+/** \name Geosynchronous orbit definitions
+ *  Requirements for an orbit to be called geosynchronous.
+ */
+///@{
 ///lower mean motion for geosynchronous satellites
 #define GEOSYNCHRONOUS_LOWER_MEAN_MOTION 0.9
 ///upper mean motion for geosynchronous satellites
@@ -14,10 +15,12 @@
 #define GEOSYNCHRONOUS_ECCENTRICITY_THRESHOLD 0.2
 ///upper inclination for geosynchronous satellites
 #define GEOSYNCHRONOUS_INCLINATION_THRESHOLD_DEGREES 70
+///@}
 
-/**
- * Mathematical convenience constants used by sgp4/sdp4 and related routines
+/** \name Mathematical constants
+ *  Mathematical convenience constants used by sgp4/sdp4 and related routines.
  */
+///@{
 /// pi
 #define PI		3.14159265358979323846
 /// pi/2
@@ -28,20 +31,24 @@
 #define THREE_PI_HALF	4.71238898038468967
 /// 2/3
 #define TWO_THIRD	6.6666666666666666E-1
+///@}
 
-/**
- * Time convenience constants
- **/
+/** \name Time constants
+ *  Constants used for time conversions.
+ */
+///@{
 ///Number of minutes per day, XMNPDA in spacetrack report #3
 #define MINUTES_PER_DAY	1.44E3
 ///Number of seconds per day
 #define SECONDS_PER_DAY	8.6400E4
 ///Difference between libpredict's predict_julian_date_t and the julian time format used in some of the internal functions
 #define JULIAN_TIME_DIFF 2444238.5
+///@}
 
-/**
- * Physical properties
- **/
+/** \name Physical properties
+ *  General physical properties and definitions.
+ */
+///@{
 ///J3 Harmonic (WGS '72), XJ3 in spacetrack report #3
 #define J3_HARMONIC_WGS72 			-2.53881E-6
 ///WGS 84 Earth radius km, XKMPER in spacetrack report #3
@@ -60,30 +67,37 @@
 #define SPEED_OF_LIGHT				299792458.0
 ///Angular velocity of Earth in radians per seconds
 #define EARTH_ANGULAR_VELOCITY			7.292115E-5
+///@}
 
-/**
- * Iteration constants
- **/
+/** \name Iteration constants
+ *  Constants used in iteration functions like predict_max_elevation(),
+ *  predict_next_aos() and predict_next_los().
+ */
+///@{
 ///Threshold used for fine-tuning of AOS/LOS
 #define AOSLOS_HORIZON_THRESHOLD 	0.3
 ///Threshold used for comparing lower and upper brackets in find_max_elevation
 #define MAXELE_TIME_EQUALITY_THRESHOLD 	FLT_EPSILON
 ///Maximum number of iterations in find_max_elevation
 #define MAXELE_MAX_NUM_ITERATIONS 	10000
+///@}
 
-/**
- * Spacetrack report #3 constants also used outside of SGP4/SDP4 code.
- * The constants/symbols are defined on page 76 to page 78 in the report.
- **/
+/** \name General spacetrack report #3 constants
+ *  These constants are also used outside of SGP4/SDP4 code.  The
+ *  constants/symbols are defined on page 76 to page 78 in the report.
+ */
+///@{
 ///k_e = sqrt(Newton's universal gravitational * mass of the Earth), in units (earth radii per minutes)^3/2
 #define XKE		7.43669161E-2
 ///Corresponds to 1/2 * J_2 * a_E^2. J_2 is the second gravitational zonal harmonic of Earth, a_E is the equatorial radius of Earth.
 #define CK2		5.413079E-4
+///@}
 
-/**
- * Spacetrack report #3 constants used only by SGP4/SDP4 code.
- * The constants/symbols are defined on page 76 to page 78 in the report.
- **/
+/** \name Specific spacetrack report #3 constants
+ *  These constants are only used by SGP4/SDP4 code.  The constants/symbols are
+ *  defined on page 76 to page 78 in the report.
+ */
+///@{
 ///Shorthand for 10^-6.
 #define E6A		1.0E-6
 ///Distance units / Earth radii.
@@ -94,16 +108,18 @@
 #define S_DENSITY_PARAM	1.012229
 ///Corresponds to (q_0 - s)^4 in units (earth radii)^4, where q_0 and s are parameters for the SGP4/SGP8 density function.
 #define QOMS2T		1.880279E-09
+///@}
 
-/**
- * Constants in deep space subroutine. Not defined in spacetrack report #3.
+/** \name Constants in deep space subroutines
+ *  Not defined in spacetrack report #3.
  *
- * The constants might originally be defined in Hujsak (1979) and/or Hujsak and
- * Hoots (1977), but this is unavailable on the Internet. Reiterated in F. R.
- * Hoots, P. W. Schumacher and R. A. Glober, "A HISTORY OF ANALYTICAL ORBIT
- * MODELING IN THE UNITED STATES SPACE SURVEILLANCE SYSTEM", 2004. Page numbers
- * below refer to this article.
- **/
+ *  The constants might originally be defined in Hujsak (1979) and/or Hujsak
+ *  and Hoots (1977), but this is unavailable on the Internet. Reiterated in F.
+ *  R.  Hoots, P. W. Schumacher and R. A. Glober, "A HISTORY OF ANALYTICAL
+ *  ORBIT MODELING IN THE UNITED STATES SPACE SURVEILLANCE SYSTEM", 2004. Page
+ *  numbers below refer to this article.
+ */
+///@{
 ///Solar mean motion (n_s in units radians/minute, p. 29)
 #define ZNS		1.19459E-5
 ///Solar perturbation coefficient (C_s in units radians/minute, p. 29)
@@ -152,5 +168,6 @@
 #define ROOT54		2.1765803E-9
 ///The time-derivative of the Greenwich hour angle in radians per minute (\dot{\theta}, used on p. 36. Not directly defined in report, but values and naming are consistent with this)
 #define THDT           4.3752691E-3
+///@}
 
 #endif
