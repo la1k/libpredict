51e1156281652bb4854eada5580f56d10e90cf95
Jens Abraham <jensab@samfundet.no>
2017-12-11 21:15:48 +0100

    Rewriting sun_predict() based on Meeus ch. 25

--- src/sun.c
+++ src/sun.c
@@ -40,24 +40,24 @@ double Degrees(double arg)
 
 void sun_predict(double time, double position[3])
 {
-	double jul_utc = time + JULIAN_TIME_DIFF;
-	double mjd = jul_utc - 2415020.0;
-	double year = 1900 + mjd / 365.25;
-	double T = (mjd + Delta_ET(year) / SECONDS_PER_DAY) / 36525.0;
-	double M = Radians(fmod(358.47583+fmod(35999.04975*T,360.0)-(0.000150+0.0000033*T)*Sqr(T),360.0));
-	double L = Radians(fmod(279.69668+fmod(36000.76892*T,360.0)+0.0003025*Sqr(T),360.0));
-	double e = 0.01675104-(0.0000418+0.000000126*T)*T;
-	double C = Radians((1.919460-(0.004789+0.000014*T)*T)*sin(M)+(0.020094-0.000100*T)*sin(2*M)+0.000293*sin(3*M));
-	double O = Radians(fmod(259.18-1934.142*T,360.0));
-	double Lsa = fmod(L+C-Radians(0.00569-0.00479*sin(O)), 2*M_PI);
-	double nu = fmod(M+C, 2*M_PI);
-	double R = 1.0000002*(1.0-Sqr(e))/(1.0+e*cos(nu));
-	double eps = Radians(23.452294-(0.0130125+(0.00000164-0.000000503*T)*T)*T+0.00256*cos(O));
-	R = ASTRONOMICAL_UNIT_KM*R;
+	double JD = time + 2444238.5; // julian date from predict time
+	double T = (JD - 2451545.0) / 36525; // julian centuries - Meeus (25.1)
+	double M = (357.52911 + 35999.05029*T-0.0001537*pow(T,2))/180*M_PI; // mean anomaly - Meeus (25.3)
+	double L_0 = (280.46646 + 36000.76983*T+0.0003032*pow(T,2))/180*M_PI; // mean longitude - Meeus (25.2)
+	double e = 0.016708634 - 0.000042037*T - 0.0000001267*pow(T,2); // eccentricyit of Earths orbit - Meeus (25.4)
+	double C = ((1.914602 - 0.004817*T - 0.000014*pow(T,2)) * sin(M) + (0.019993 - 0.000101 * T) * sin(2*M) + 0.000289 * sin(3*M))/180*M_PI; // Sun's equation of center - Meeus p. 164
+	double L = L_0 + C; // Sun's true longitude
+	double nu = M+C;	// Sun's true anomaly
+	double R = 1.000001018*(1-pow(e,2)) / (1 + e * cos(nu)); // Sun's radius vector - Meeus (25.5)
+	// result here is the true geometric longitude referred to the mean equinox of the date
+	R = ASTRONOMICAL_UNIT_KM*R; // R in AU to R in km
+	const double eps = 23.43928 / 180 * M_PI; // obliquity of the ecliptic at J2000.0
+	// TODO: check if we need to calc eps as mean obliquiy of the ecliptic and not just using the constant there
 
-	position[0] = R*cos(Lsa);
-	position[1] = R*sin(Lsa)*cos(eps);
-	position[2] = R*sin(Lsa)*sin(eps);
+	// rectangular coordinates of the sun reffered to the mean equator and equinox of the date - Meeus (26.1)
+	position[0] = R*cos(L);
+	position[1] = R*sin(L)*cos(eps);
+	position[2] = R*sin(L)*sin(eps);
 }
 
 void predict_observe_sun(const predict_observer_t *observer, double time, struct predict_observation *obs)
