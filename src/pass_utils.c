ecdb18b60e291f25c7d514939fd07bf5001b2b0c
Asgeir Bjørgan <asgeirbj@samfundet.no>
2017-12-23 14:59:31 +0100

    Initial implementation of AOS/LOS finding.

--- src/pass_utils.c
+++ src/pass_utils.c
@@ -1,4 +1,6 @@
 #include "pass_utils.h"
+#include <math.h>
+#include <float.h>
 
 void observe_orbit_at(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, struct predict_observation *observation)
 {
@@ -7,43 +9,44 @@ void observe_orbit_at(const predict_observer_t *observer, const predict_orbital_
 	predict_observe_orbit(observer, &position, observation);
 }
 
-predict_julian_date_t step_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction)
+predict_julian_date_t step_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction, enum step_pass_type type)
 {
 	struct predict_observation observation;
 	observe_orbit_at(observer, orbital_elements, curr_time, &observation);
 
-	if (observation.elevation < 0) {
-		return curr_time;
+	int elevation_sign = 1;
+	if (type == STEP_UNTIL_PASS) {
+		elevation_sign = 1;
+	} else if (type == STEP_OUT_OF_PASS) {
+		elevation_sign = -1;
 	}
 
-	return 0;
-}
-
-predict_julian_date_t step_until_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction)
-{
-	struct predict_observation observation;
-	observe_orbit_at(observer, orbital_elements, curr_time, &observation);
-
-	if (observation.elevation > 0) {
+	if (observation.elevation*elevation_sign > 0) {
 		return curr_time;
 	}
 
 	double mean_period = 1.0/orbital_elements->mean_motion;
 
-	const int NUM_POINTS = 4;
-	double step = mean_period/NUM_POINTS;
-	struct predict_observation observations[NUM_POINTS];
-	bool found_pass = false;
-	while (!found_pass) {
-		for (int i=0; i < NUM_POINTS; i++) {
-			curr_time += step;
-			observe_orbit_at(observer, orbital_elements, curr_time, &observations[i]);
-		}
-		found_pass = true;
+	// TODO: Predict orbits at one fourth of the mean period. Calculate rough estimate of the local maximas and minimas along
+	// the periods, readjust starting time and then predict at one fourths again. Use this to efficiently iterate until pass start.
+	// See commit 1a6da2f402c40cb71d7b79edf8087d9d01f4db7d for starting point for this.
 
+	double step = fabs(mean_period/8.0);
+	if (direction == NEGATIVE_DIRECTION) {
+		step = -step;
 	}
 
-	return 0;
+	bool found_pass = false;
+
+	while (!found_pass) {
+		curr_time += step;
+		observe_orbit_at(observer, orbital_elements, curr_time, &observation);
+		if (observation.elevation*elevation_sign > 0) {
+			found_pass = true;
+		}
+	}
+
+	return curr_time;
 }
 
 bool aoslos_is_feasible(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t start_utc)
@@ -60,9 +63,7 @@ struct predict_observation next_aos_los(const predict_observer_t *observer, cons
 		return observation;
 	}
 
-	struct predict_position orbit = {0};
-	predict_orbit(orbital_elements, &orbit, start_utc);
-	predict_observe_orbit(observer, &orbit, &observation);
+	observe_orbit_at(observer, orbital_elements, start_utc, &observation);
 
 	//find lower and upper brackets for solution
 	predict_julian_date_t lower_bracket;
@@ -70,22 +71,44 @@ struct predict_observation next_aos_los(const predict_observer_t *observer, cons
 
 	if (search_type == AOS_SEARCH) {
 		if (observation.elevation > 0) {
-			lower_bracket = step_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION);
+			lower_bracket = step_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION, STEP_OUT_OF_PASS);
 		} else {
 			lower_bracket = start_utc;
 		}
-		upper_bracket = step_until_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION);
+		upper_bracket = step_pass(observer, orbital_elements, lower_bracket, POSITIVE_DIRECTION, STEP_UNTIL_PASS);
 	} else {
 		if (observation.elevation > 0) {
 			lower_bracket = start_utc;
 		} else {
-			lower_bracket = step_until_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION);
+			lower_bracket = step_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION, STEP_UNTIL_PASS);
 		}
-		upper_bracket = step_pass(observer, orbital_elements, start_utc, POSITIVE_DIRECTION);
+		upper_bracket = step_pass(observer, orbital_elements, lower_bracket, POSITIVE_DIRECTION, STEP_OUT_OF_PASS);
 	}
 
-	predict_orbit(orbital_elements, &orbit, lower_bracket + (upper_bracket - lower_bracket)/2.0);
-	predict_observe_orbit(observer, &orbit, &observation);
+	const double AOSLOS_TIME_EQUALITY_THRESHOLD = FLT_EPSILON;
+	const int AOSLOS_MAX_NUM_ITERATIONS = 1000;
+	int iteration = 0;
 
-	return observation;
+	predict_julian_date_t time_candidate;
+	struct predict_observation candidate, lower, upper;
+
+	while ((fabs(lower_bracket - upper_bracket) > AOSLOS_TIME_EQUALITY_THRESHOLD) && (iteration < AOSLOS_MAX_NUM_ITERATIONS)) {
+		time_candidate = (upper_bracket + lower_bracket)/2.0;
+
+		observe_orbit_at(observer, orbital_elements, time_candidate, &candidate);
+		observe_orbit_at(observer, orbital_elements, lower_bracket, &lower);
+		observe_orbit_at(observer, orbital_elements, upper_bracket, &upper);
+
+		if (candidate.elevation*lower.elevation < 0) {
+			upper_bracket = time_candidate;
+		} else if (candidate.elevation*upper.elevation < 0) {
+			lower_bracket = time_candidate;
+		} else {
+			break;
+		}
+		iteration++;
+	}
+
+	observe_orbit_at(observer, orbital_elements, (upper_bracket + lower_bracket)/2.0, &candidate);
+	return candidate;
 }
--- src/pass_utils.h
+++ src/pass_utils.h
@@ -8,19 +8,9 @@
  **/
 enum step_pass_direction{POSITIVE_DIRECTION, NEGATIVE_DIRECTION};
 
-/**
- * Rough stepping through a pass.
- *
- * \param observer Ground station
- * \param orbital_elements Orbital elements of satellite
- * \param curr_time Time from which to start stepping
- * \param direction Either POSITIVE_DIRECTION (step from current time to pass end) or NEGATIVE_DIRECTION (step from current time to start of pass). In case of the former, the pass will be stepped until either elevation is negative or the derivative of the elevation is negative
- * \return Time for when we have stepped out of the pass
- **/
-predict_julian_date_t step_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction);
+enum step_pass_type{STEP_UNTIL_PASS, STEP_OUT_OF_PASS};
 
-predict_julian_date_t step_until_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction);
-
+predict_julian_date_t step_pass(const predict_observer_t *observer, const predict_orbital_elements_t *orbital_elements, predict_julian_date_t curr_time, enum step_pass_direction direction, enum step_pass_type type);
 
 enum aos_los_search {
 	AOS_SEARCH,
